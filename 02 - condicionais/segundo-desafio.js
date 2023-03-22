b7292d0c370d4ecccecb8344bf0ae7114cb54df8
gabrielly <gabriellycristiny16@gmail.com>
2023-03-21 21:10:31 -0300

    segundo-desafio-js

---
+++ 02 - condicionais/segundo-desafio.js
@@ -0,0 +1,11 @@
+const numero = 19;
+const numeroPar = (numero % 5) === 0;
+console.log(numeroPar);
+
+if (numeroPar) {
+    console.log('Executei');
+} else {(!numeroPar) 
+    console.log('Impar')
+
+}
+
