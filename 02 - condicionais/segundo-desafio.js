94bd1b6098de06b7e7a88646428b5e1a59c1e69e
gabrielly <gabriellycristiny16@gmail.com>
2023-03-24 11:17:15 -0300

    segundo-desafio

--- 02 - condicionais/segundo-desafio.js
+++ 02 - condicionais/segundo-desafio.js
@@ -2,10 +2,21 @@ const numero = 19;
 const numeroPar = (numero % 5) === 0;
 console.log(numeroPar);
 
+
+if(numero === 0){
+    console.log('O número é inválido');
+} else if(numeroDivisielPor5){
+    console.log('sim');
+} else {
+    console.log('não');
+}
+
+
 if (numeroPar) {
     console.log('Executei');
 } else {(!numeroPar) 
     console.log('Impar')
+} 
 
-}
+
 
